ba1bbca0d4ba02e962c305b0f33bf45d191e6168
william ariza <william.ariza@mercadolibre.com>
2022-05-17 22:19:45 -0500

    feat: test server and client

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    exclude: [...configDefaults.exclude, '**/src/api/**', '**/main.tsx/**'],
+  },
 })
