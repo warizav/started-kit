b572211824994dbf6a0efadaa579c957be10a6df
William Ricardo Ariza Velez <wilarizav@gmail.com>
2023-12-17 16:23:37 -0500

    feature: update start kit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { configDefaults } from 'vitest/config'
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    exclude: [...configDefaults.exclude, '**/src/api/**', '**/main.tsx/**'],
+    exclude: [...configDefaults.exclude, '**/src/api/**', '**/main.tsx/**']
   },
-})
+  build: {
+    outDir: path.join(__dirname, 'dist/src/client')
+  }
+});
